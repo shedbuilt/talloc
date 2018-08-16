180674e5e0c0889a200338048f9d5a9026f95e56
Auston Stewart <auston@shedbuilt.net>
2018-08-15 20:30:23 -1000

    Updating to 2.1.14 and making python an explicit dependency

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./configure --prefix=/usr &&
+./configure --prefix=/usr \
+            --extra-python=/usr/bin/python3 &&
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
